8f833957cb1b7cd492dfa07abddca2feb2f2d8a4
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2017-12-16 22:36:50 -0200

    add file upload

--- src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
+++ src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -9,34 +9,37 @@ import javax.enterprise.inject.Model;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
+import javax.servlet.http.Part;
 import javax.transaction.Transactional;
 
 @Model
 public class AdminLivrosBean {
 
     private Livro livro = new Livro();
-    
+
     @Inject
     private LivroDAO livroDAO;
     @Inject
     private AutorDAO autorDAO;
     @Inject
     private FacesContext context;
-    
+
+    private Part capaLivro;
+
     @Transactional
     public String salvar() {
         livroDAO.salvar(livro);
-        
+
         context.getExternalContext().getFlash().setKeepMessages(true);
         context.addMessage(null, new FacesMessage("Livro cadastrado com sucesso"));
-        
+
         return "/livros/lista?faces-redirect=true";
     }
 
     public List<Autor> getAutores() {
         return autorDAO.listar();
     }
-    
+
     public Livro getLivro() {
         return livro;
     }
@@ -44,5 +47,13 @@ public class AdminLivrosBean {
     public void setLivro(Livro livro) {
         this.livro = livro;
     }
-    
+
+    public Part getCapaLivro() {
+        return capaLivro;
+    }
+
+    public void setCapaLivro(Part capaLivro) {
+        this.capaLivro = capaLivro;
+    }
+
 }
